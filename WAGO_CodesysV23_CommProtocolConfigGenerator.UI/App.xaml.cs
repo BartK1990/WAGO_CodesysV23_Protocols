d18d51985f8fc9a538ed03382710fdf6e61205f6
Bartosz Kuriata <kuriata.bartosz@gmail.com>
2021-10-13 21:59:04 +0200

    Add project files.

---
+++ WAGO_CodesysV23_CommProtocolConfigGenerator.UI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WAGO_CodesysV23_CommProtocolConfigGenerator.UI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
